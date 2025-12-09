8dde7e21424a08adff6693fbe33a66089deb224b
Pathomphong2607 <pzt0935e@gmail.com>
2025-12-09 12:06:02 +0700

    Add files via upload

---
+++ Projects/Python/68_algorithm/week5/tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+
+if(type(Name == str)):
+    print("True",Name)
+else:
+    print("False",Name)
+if(type(Idstu == complex)):
+    print("True",Idstu)
+else:
+    print("False",Idstu)
+if(type(Gpax == float)):
+    print("True",Gpax)
+else:
+    print("False",Gpax)
+if(type(Age == int)):
+    print("True",Age)
+else:
+    print("False",Age)
---
+++ Projects/Python/68_algorithm/week5/type.py
@@ -0,0 +1,23 @@
+Name = ("Pathomphong Phuengprakhon")
+Idstu = 684245008
+Gpax = 3.50
+Age = 98
+
+
+if(type(Name) == str):
+    print("True",Name)
+else:
+    print("False",Name)
+if(type(Idstu) == complex):
+    print("True",Idstu)
+else:
+    print("False",Idstu)
+if(type(Gpax) == float):
+    print("True",Gpax)
+else:
+    print("False",Gpax)
+if(type(Age) == int):
+    print("True",Age)
+else:
+    print("False",Age)
+
